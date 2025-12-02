b4cc6c6e0127d8ba2bfedffec6ecb1b0456bfdb6
rzkinr <rizkynurrokhim18@gmail.com>
2025-12-02 10:59:10 +0700

    using firebase on cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {useSelector} from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,6 +7,22 @@ import Products from './components/Shop/Products';
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    // This effect could be used to sync cart data with a backend
+    // For example, sending cart data to a server whenever it changes
+    fetch(
+      'https://realtime-database-baae8-default-rtdb.firebaseio.com/cart.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      },
+    ).catch((error) => {
+      console.error('Error updating cart data:', error);
+    });
+    console.log('Cart updated:', cart);
+  }, [cart]);
 
   return (
     <Layout>
